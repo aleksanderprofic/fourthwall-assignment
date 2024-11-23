9eae950aa2b54e5fc23d6f531206c45d4d081b35
aleksanderprofic <aleksanderprofic@gmail.com>
2024-11-23 17:16:21 +0100

    Minimal working spring application

---
+++ src/main/kotlin/com/fourthwall/assignment/FourthwallAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.fourthwall.assignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FourthwallAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<FourthwallAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/fourthwall/assignment/FourthwallAssignmentApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.fourthwall.assignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FourthwallAssignmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
